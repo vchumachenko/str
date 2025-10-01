84266e1403c9a5e9d4f78474f5522c52e2520dfb
Vasily Chumachenko <v.chumachenko@yahoo.com>
2025-10-01 18:21:33 +0300

    Initial commit

---
+++ src/main/java/com/example/str/StrApplication.java
@@ -0,0 +1,13 @@
+package com.example.str;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrApplication.class, args);
+    }
+
+}
